39040ea95bebb055fcf4d4ab57bea34c56558e65
Channrith Moul <chan@Chans-MacBook-Pro.local>
2020-09-28 22:30:44 +0700

    Updates

---
+++ precache-manifest.bec96cb168be566cc37580f4d9207c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397ad3df9db03a7893e5fa45916f52b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d71a65ae0fb03c926ab5",
+    "url": "/monsters-rolodex/static/css/main.20664a60.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71a65ae0fb03c926ab5",
+    "url": "/monsters-rolodex/static/js/main.6a86d09f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
